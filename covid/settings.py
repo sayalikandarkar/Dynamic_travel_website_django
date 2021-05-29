bcf8843fbbf038aa55e3676fcdd281ad290597fb
Sayali <sayalikandarkar.io@gmail.com>
2021-05-29 13:20:48 +0530

    updating key for settings.py

--- covid/settings.py
+++ covid/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=t-mk@#-tfig6006ln6=^*ul7%rffy_=&xp1_ucr!mfd7=tc*5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
